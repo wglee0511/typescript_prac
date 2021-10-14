36d369c905216eec2943e2f9a2154b728f96a4d2
wglee0511 <ranazang@naver.com>
2021-10-14 14:09:30 +0900

    class로 대체하기(인터페이스)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "h",
-  age: 23,
-  gender: "male",
-};
+const person = new Human("g", 23, "male");
 
 const sayHi = (person: Human): string => {
   return `${person.name}, ${person.age}, ${person.gender}`;
