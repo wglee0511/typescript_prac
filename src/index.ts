77e5db6a5ca92c0c70dded5833042d355ceb18c8
wglee0511 <ranazang@naver.com>
2021-10-14 13:54:53 +0900

    인터페이스 사용하기

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-const sayHi = (name: string, age: number, gender?: string): string => {
-  return `${name}, ${age}, ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "h",
+  age: 23,
+  gender: "male",
+};
+
+const sayHi = (person: Human): string => {
+  return `${person.name}, ${person.age}, ${person.gender}`;
 };
 
 export {};
